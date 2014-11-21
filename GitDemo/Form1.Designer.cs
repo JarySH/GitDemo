ab853e634567ea550babe7788b3383c6c383a7fc
SteveLuo <supersky@steveluo.name>
2014-11-21 10:05:47 +0800

    创建了Load事件

--- GitDemo/Form1.Designer.cs
+++ GitDemo/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.ClientSize = new System.Drawing.Size(700, 463);
             this.Name = "Form1";
             this.Text = "更改了标题";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
